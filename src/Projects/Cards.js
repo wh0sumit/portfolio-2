3496e0371d3de70d1b0fccd508e0e25b3e822658
yashsehgal <yashsehgal.work@gmail.com>
2021-07-01 22:59:59 +0530

    images feature fixed :rocket:

--- src/Projects/Cards.js
+++ src/Projects/Cards.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import './Projects.css'
-import ProjectsData from './ProjectsData.json'
+import ProjectsImageData from './ProjectsImageData'
 
-
-function Cards() {
-  
-  
-     return (
-       <>
-       {
-         ProjectsData.map ((Projects)=>{
-           return <div class="col">
-                      <div class="card bg-1 h-100 shadow-sm">
-                        {/* <img src={require(Projects.img)} alt="" /> */}
-                        <div class="card-body">
-                          <p class="card-text">{Projects.desc}</p>
-                        </div>
-                        <div class="d-flex justify-content-between align-items-center card-footer">
-                              {/* <a class=".btn-link .btn-link-custom-1" target="_blank">View</a> */}
-                              <button class="btn-link px-3 py-1 rounded-3">View</button>
-                            <small class="text-muted">03.2021</small>
-                          </div>
-                      </div>
-                  </div>
-         })
-       }
-      </>
-     )
+function Cards(ProjectDetails) {
+  return (
+    <div class="col">
+      <div class="card bg-1 h-100 shadow-sm">
+        <img src={ProjectsImageData[ProjectDetails.ProjectTitle]} alt="" />
+        <div class="card-body">
+          <p class="card-text">{ProjectDetails.desc}</p>
+        </div>
+        <div class="d-flex justify-content-between align-items-center card-footer">
+          <a class=".btn-link .btn-link-custom-1" target="_blank">View</a>
+          <button class="btn-link px-3 py-1 rounded-3">View</button>
+          <small class="text-muted">03.2021</small>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Cards
